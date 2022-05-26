90a763846a88d1471dc19789788ce1e628ea021a
cmosinverter <61655399+cmosinverter@users.noreply.github.com>
2022-05-26 21:08:29 +0800

    Update floyd_worshall.py

--- floyd_worshall.py
+++ floyd_worshall.py
@@ -2,8 +2,8 @@
 import pandas as pd
 
 inf = float("inf")
-A = [[0, 3, inf, 7], [8, 0, 2, inf], [5, inf, 0, 1], [2, inf, inf, 0]]
-n = 4
+A = [[0, 3, inf, 7], [8, 0, 2, inf], [5, inf, 0, 1], [2, inf, inf, 0]] # adjacency matrix
+n = 4 # number of nodes
 
 for k in range(n):
     for i in range(n):
